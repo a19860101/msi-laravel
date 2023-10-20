3106aec63719e6227d76ad8382a125f8f83bdc22
Zac <a19860101@gmail.com>
2023-10-20 20:59:09 +0800

    20231020

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,5 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// post
+Route::get('/post/create',[PostController::class,'create'])->name('post.create');
 
 require __DIR__.'/auth.php';
